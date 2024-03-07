74208d8a3e389703b343ef74580629551c50f67f
Bodi Szabo <bodszab@nuevaschool.org>
2024-03-06 23:59:39 -0800

    minor change

--- test_application/examplescript.py
+++ test_application/examplescript.py
@@ -1,3 +1,3 @@
 from sequential_loading.data_storage import SQLStorage
 
-my_storage = SQLStorage("postgresql://bodszab@localhost:5432/teststorage", createdb=True)
+my_storage = SQLStorage("postgresql://username@localhost:5432/teststorage", createdb=True)
