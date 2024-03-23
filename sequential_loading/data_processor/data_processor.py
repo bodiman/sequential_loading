9ae08759bd3cce1e0d57f1e7846212da175098b4
Bodi Szabo <bodszab@nuevaschool.org>
2024-03-23 16:45:00 -0700

    Generalized functions from IntervalProcessor to DataProcessor parent class

--- sequential_loading/data_processor/data_processor.py
+++ sequential_loading/data_processor/data_processor.py
@@ -53,14 +53,60 @@ class DataProcessor(ABC):
         #replaced "and" with "&" to match pandas documentation (I think). Change back if this breaks something.
         parameter_query = ' & '.join([f'{key} == "{value}"' for key, value in parameters.items()])
         return parameter_query
+    
+    #this function is actually not unique to IntervalProcessor, but could be used by all DataProcessors
+    #The reason we pass in data as a dataframe is to preserve typing
+    def update_metadata(self, parameters: pd.DataFrame, metadata: pd.DataFrame, deletion: bool = False) -> None:
+        assert len(parameters) == 1, "Parameters must be a single row dataframe."
+        assert len(metadata) == 1, "Metadata must be a single row dataframe."
+
+        metadata = pd.concat([parameters, metadata], axis=1)
+        self.metaschema(metadata)
+
+        #cache metadata to avoid unnecessary queries to storage
+        if self.cached_metadata is None:
+            self.cached_metadata = metadata
+            return metadata
+        
+        parameters = parameters.iloc[0].to_dict()
+
+        cached_metadata, metadata_index = self.retrieve_metadata(**parameters)
+
+        if cached_metadata is None:
+            self.cached_metadata = pd.concat([self.cached_metadata, metadata], ignore_index=True)
+            return metadata
+
+        for key, value in self.update_map.items():
+            cached_metadata[key] = value(cached_metadata[key], metadata.loc[0, key], deletion)
+        
+        #Get index of existing metadata
+        self.cached_metadata.loc[metadata_index] = cached_metadata
+
+        return cached_metadata
+
+    #This is also not specific to IntervalProcessor, but could be used by all DataProcessors
+    def update_data(self, parameters: pd.DataFrame, data: Type[TypedDataFrame]) -> pd.DataFrame:  
+        data = pd.concat([parameters, data], axis=1)
+        self.schema(data)
+        self.data = data
+
+        return self.data
+    
+    #also not specific, will move all these eventually
+    def retrieve_metadata(self, **parameters: dict) -> dict:
+        if self.cached_metadata is None:
+            return (None, None)
+
+        parameter_query = ' & '.join([f'{key} == "{value}"' for key, value in parameters.items()])
+        results = self.cached_metadata.query(parameter_query)
+        
+        if results.empty:
+            return (None, None)
+        
+        return results.iloc[0], results.index[0]
 
     @abstractmethod
     def initialize(self, **parameters: dict) -> None:
-        pass
-
-    "Updates metadata"
-    @abstractmethod
-    def update_metadata(self, parameters: Type[TypedDataFrame], metadata: Type[TypedDataFrame]) -> None:
         pass
     
     @abstractmethod
--- test_application/examplescript.py
+++ test_application/examplescript.py
@@ -62,5 +62,5 @@ class EODSchema(TypedDataFrame):
 tiingo_collector = tiingoCollector("TIINGO", api_key=api_key)
 
 processor = IntervalProcessor("stock_processor", EODParamSchema, EODSchema, my_storage, unit="days")
-# processor.collect([tiingo_collector], ticker="AAPL", domain="/2020-01-01|2021-02-01")
-# processor.delete([tiingo_collector], ticker="SPY", domain="/2020-01-01|2021-01-01")
+processor.collect([tiingo_collector], ticker="IWF", domain="/2020-01-01|2021-02-01")
+# processor.delete([tiingo_collector], ticker="SPY", domain="/2020-01-01|2021-02-01")
